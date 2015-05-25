a94f783d0c52502d3370f284a14a38e03b2a2864
Quentin Delance <quentin.delance@gmail.com>
2015-05-25 08:55:47 +0200

    Add new sample module for theme

---
+++ sandwich/src/Controller/SandwichController.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\sandwich\Controller\SandwichController.
+ */
+
+namespace Drupal\sandwich\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Returns responses for sandwich module routes.
+ */
+class SandwichController extends ControllerBase {
+
+  /**
+   * Builds a sandwich.
+   */
+  public function build() {
+
+    $best_sandwich = [
+      '#theme' => 'sandwich',
+      '#name' => $this->t('Chickado'),
+      '#attributes' => [
+        'id' => 'best-sandwich',
+        'style' => 'float: left;',
+        'class' => ['left', 'clearfix'],
+      ],
+      '#bread' => $this->t('Sourdough'),
+      '#cheese' => $this->t('Gruyère'),
+      '#veggies' => [
+        $this->t('Avocado'),
+        $this->t('Red onion'),
+        $this->t('Romaine'),
+      ],
+      '#protein' => $this->t('Chicken'),
+      '#condiments' => [
+        $this->t('Mayo'),
+        $this->t('Dijon'),
+      ],
+    ];
+
+    $alt_best_sandwich = [
+      '#theme' => 'sandwich',
+      '#name' => $this->t('Vegan'),
+      '#attributes' => [
+        'id' => 'alt-sandwich',
+        'style' => 'float: right;',
+        'class' => ['right', 'clearfix'],
+      ],
+      '#bread' => $this->t('Multigrain'),
+      '#veggies' => [
+        $this->t('Avocado'),
+        $this->t('Tomato'),
+        $this->t('Arugula'),
+      ],
+      '#protein' => $this->t('Faken'),
+      '#condiments' => [
+        $this->t('Dijon'),
+        $this->t('Vegan Mayo'),
+      ],
+    ];
+    return [
+      '#title' => 'Menu',
+      '#type' => 'container',
+      '#attached' => ['library' => ['sandwich/flavour']],
+      '#attributes' => ['class' => ['admin']],
+      $best_sandwich,
+      $alt_best_sandwich,
+    ];
+  }
+
+}
